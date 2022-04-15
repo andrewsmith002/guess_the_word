25a0593fa83f84074875387cc6eb4ad3a5ca95ff
andrewsmith002 <andrew_smith002@yahoo.ie>
2022-04-15 22:01:10 +0000

    Created a list of the programming languages for the user to guess and defined variables

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#importing the libraries
+import random
+
+
+#The list of programming languages to guess for the game.
+guess_the_language_list = ['html','java', 'javascript', 'python', 'ruby', 'sql', 'php', 'css', 'c++']
+
+
+#Generates a random language from the list of languages that is to be guessed by the user
+language = random.choice(guess_the_language_list)
+#
+language_guessed = list('*'*len(language))
+#Defined 
+characters_guessed=[]
