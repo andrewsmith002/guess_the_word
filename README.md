b12bfcfac682ea178986d772c145e1025d0cd872
andrewsmith002 <andrew_smith002@yahoo.ie>
2022-04-16 05:48:35 +0000

    Amendment made

--- README.md
+++ README.md
@@ -1,31 +1,95 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# Guess the Word 
 
-Welcome andrewsmith002,
+Guess the Word is a Python terminal game, which runs in the Code Institute mock terminal on Heroku.
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
+This game, guess the word, is based on programming languages.  Users have to guess what language is hidden behind asterisks.
 
-## Reminders
+<br>
 
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
+Here is a link to my project: https://guess-the-language001.herokuapp.com/
+<br>
 
-## Creating the Heroku app
+## How to play
+<br>
+Guess the Word is a game similar to hangman but without the image of the hangman.  
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+In this version, the player will choose how difficult the level will be by entering how many attempts he or she requires.
 
-1. `heroku/python`
-2. `heroku/nodejs`
+The player will be able to see the length of the language but will be marked out with asterisks.
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+If the player enters a correct character or symbol, the asterisk will disappear where that characte or symbol is.
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+There will also be a message displaying that the player got it correct and will show what letters or characters have been already guessed.
 
-Connect your GitHub repository and deploy as normal.
+If the player gets the language wrong a message will be displayed saying that its incorrect.  Also there will be a display message indocated how many failed attempts have been made.
 
-## Constraints
+If the player thinks he/she knows the language they can simply type the word. 
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+<br>
 
------
-Happy coding!
+## Features
+<br>
+
+### Exisiting Features
+<br>
+There is a list of languages in the programme and one language word is randomly assigned for the player to guess.
+
+The player cannot see what languages are contained in the list, however, the player will is able to see the length of the word by the number of asterisks.
+
+<img src="screenshot.png"> 
+<br>
+
+## Testing
+<br>
+
+I manually tested this project by doing the following:
+
+<ul> Passed the code through a PEP8 linter and has minor issues</ul>
+<ul>Given invalid inputs where the user is asked to enter number of attempts</ul>
+<ul>Tested in my local terminal and the Code Institute Heroku terminal without any problems</ul>
+<br>
+
+## Bugs
+<br>
+
+### Solved Bugs
+<br>
+
+<ul>When I wrote the project, I was getting index errors </ul>
+
+## Remaining Bugs
+<br>
+<ul> No bugs remaining</ul>
+<br>
+
+## Validator Testing
+<br>
+
+<ul>PEP8</ul>
+    <ul>52 errors were returned from PEP8online.com</ul>
+These 52 errors consisted of line too long, missing whitespace, too many black rows, block comment should start with '#'.  These errors were reduce to 22 errors. The 22 remaining errors can be broken down into 20 line too long and 2 missing whitespace. When I tried to but a space in the validator indicates it creates an error when i run the code in the gitpod terminal, therefore that error can be ignored. In relation to the erros for line too long, when i break the line up and run it in the terminal in gitpod I get an error back, therefore those erros can be ignored.<br>
+<br>
+
+## Deployment
+<br>
+This project was deployed using Code Institute's mock terminal for Heroku.
+<br>
+<ul>Steps for deployment
+
+<ul>Clone repository</ul>
+<ul>Create a new Heroku app</ul>
+<ul>Add Var Config using PORT as the Key and 8000 as the Value</ul>
+<ul>Set the buildbacks to Python and NodeJS in that order</ul>
+<ul>Link the Heroku app to the repository</ul>
+<ul>Click on Deploy manually</ul>
+</ul>
+<br>
+
+## Credits
+<br>
+
+<ul>Code Institute for the development terminal</ul>
+<ul>w3schools.com</ul>
+<ul>https://www.geeksforgeeks.org/number-guessing-game-in-python/</ul>
+<ul>Youtube.com
+<ul>https://www.youtube.com/watch?v=m4nEnsavl6w&t=142s</ul></ul>
--- run.py
+++ run.py
@@ -1,39 +1,37 @@
-#importing the libraries
+# importing the libraries
 import random
 
 
-#The list of programming languages to guess for the game.
-guess_the_language_list = ['html','java', 'javascript', 'python', 'ruby', 'sql', 'php', 'css', 'c++']
+# The list of programming languages to guess for the game.
+guess_the_language_list = ['html', 'java', 'javascript', 'python', 'ruby', 'sql', 'php', 'css', 'c++']
 
 
-#Generates a random language from the list of languages that is to be guessed by the user
+# Generates a random language from the list of languages that is to be guessed by the user
 language = random.choice(guess_the_language_list)
-#
 language_guessed = list('*'*len(language))
-#Defined 
-characters_guessed=[]
+characters_guessed = []
 
 
-#This function reveals the '*' with each guessed character.
+# This function reveals the '*' with each guessed character.
 def reveal(guess):
-    revealing_indices=[index for index,value in enumerate(language) if value==guess]
+    revealing_indices = [index for index, value in enumerate(language) if value == guess]
     global language_guessed
     for i in revealing_indices:
-            language_guessed[i]=guess
+            language_guessed[i] = guess
 
 
-#Welcome message printed to the screen.
+# Welcome message printed to the screen.
 print('Welcome to guess the programming language\n')
 print('The programming language is: {}\n'.format(''.join(language_guessed)))
 
 
-#A print statement informing the user of the maximum failed attempts they have.
+# A print statement informing the user of the maximum failed attempts they have.
 print("You have a maximum of 5 failed attempts to guess what programming language is behind the above asterisks")
 
 
-#Created a While Not loop and a print statement  requesting the user to enter how many failed attempts they want between 1-5.
+# Created a While Not loop and a print statement  requesting the user to enter how many failed attempts they want between 1-5.
 i = 0
-while not int(i) in range(1,6):
+while not int(i) in range(1, 6):
     try:
         i = int(input("How difficult do you want to make the game?\nChoose the number of attempts you think you need to guess the hidden programming language [1-5]"))
     except:
@@ -41,43 +39,36 @@ while not int(i) in range(1,6):
 failed_attempts = 1
 
 
-
-
-#Created a While loop
-while failed_attempts<=i:
-    #Print statement containing join()methods and a format()mentod
-    print('\nGuess to reveal the programming language: ',''.join(language_guessed))
+# Created a While loop
+while failed_attempts <=i:
+    # Print statement containing join()methods and a format()mentod
+    print('\nGuess to reveal the programming language: ', ''.join(language_guessed))
     print('Previous letters guessed: ', ','.join(characters_guessed))
     guess = input("Failed attempt number: {}\nPlease enter the character or guess the language here:\n".format(failed_attempts))
 
-
-
-    #Created an IF statement when the user inputs a character that was already inputed to display a message.
+    # Created an IF statement when the user inputs a character that was already inputed to display a message.
     if guess in characters_guessed:
         print("You already tried that letter, please choose another letter that you have not chosen\n")
         continue
     characters_guessed.append(guess)
 
-
-    #Created an IF statement when the user inputs the correct programming language a message with be displayed with the correct language.
+    # Created an IF statement when the user inputs the correct programming language a message with be displayed with the correct language.
     if guess == language:
         print('You guessed the correct language\nThe programming language is: "{}"'.format(language))
         print("Congratulations!")
         exit(0)
-        
-    #Created an IF statement to display when the user inputs a correct character in the programming language and the reveal() function will display the correct character.
+
+    # Created an IF statement to display when the user inputs a correct character in the programming language and the reveal() function will display the correct character.
     if guess in language and len(guess)!=0:
         print('Correct, the letter "{}" is in this programming language'.format(guess))
         reveal(guess)
 
-
-    #Else statement created and will execute if the above IF statement is incorrect.
+    # Else statement created and will execute if the above IF statement is incorrect.
     else:
         print('Incorrect, please try again')
         failed_attempts+=1
 
-
-    #Created an IF statement that will display a print statement if the user inputs the correct programming language and an exit function.
+    # Created an IF statement that will display a print statement if the user inputs the correct programming language and an exit function.
     if '*' not in language_guessed:
         print('Congratulations, you have guessed the correct programming language, the language is: "{}"'.format(language))
         exit(0)
